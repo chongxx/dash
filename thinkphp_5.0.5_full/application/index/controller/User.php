229c11758b8ff0b5ce1756811babc6f10cb96824
chongxx <choongzhaoo@foxmail.com>
2017-05-24 00:44:46 +0800

    test: test connection mysql

--- thinkphp_5.0.5_full/application/index/controller/User.php
+++ thinkphp_5.0.5_full/application/index/controller/User.php
@@ -9,7 +9,30 @@
 namespace app\index\controller;
 
 
+use think\Db;
+use think\Request;
+
 class User
 {
+    public function create()
+    {
+        $req = Request::instance();
+        $id = $req->param('id');
+        $name = $req->param('name');
+        $user = ['id' => $id, 'name' => $name];
+        $result = Db::table('user')->insert($user);
+
+        if ($result == 1) {
+            return "add user success";
+        } else {
+            return "add user fail";
+        }
+    }
+
+    public function getAll()
+    {
+        $users = Db::table('user')->select();
+        return json_encode($users);
+    }
 
 }
