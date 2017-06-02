271d6b21fecc940661e1361ce9d2d176d039f5ec
chongxx <choongzhaoo@foxmail.com>
2017-06-02 23:44:24 +0800

    chore: create table user

--- thinkphp_5.0.5_full/public/install/start.sql
+++ thinkphp_5.0.5_full/public/install/start.sql
@@ -1,20 +1,40 @@
-
 -- ----------------------
 -- 文章
 -- ----------------------
 DROP TABLE IF EXISTS `article`;
-CREATE TABLE `article`(
-  `aid` int(10) NOT NULL AUTO_INCREMENT,
-  `title` char(255) NOT NULL DEFAULT 'no title',
-  `author` varchar(15) NOT NULL DEFAULT 'no author',
-  `content` mediumtext NOT NULL,
-  `keywords` varchar(256) NOT NULL DEFAULT '',
-  `desc` varchar(256) NOT NULL DEFAULT '',
-  `is_show` tinyint(1) NOT NULL DEFAULT '1',
-  `is_delete` tinyint(1) NOT NULL DEFAULT '0',
-  `is_top` tinyint(1) NOT NULL DEFAULT '0',
-  `views` int(10) NOT NULL DEFAULT '0',
-  `time` int(10) NOT NULL DEFAULT '0',
-  `cid` tinyint(2) NOT NULL DEFAULT '0',
+CREATE TABLE `article` (
+  `aid`       INT(10)      NOT NULL AUTO_INCREMENT,
+  `title`     CHAR(255)    NOT NULL DEFAULT 'no title',
+  `author`    VARCHAR(15)  NOT NULL DEFAULT 'no author',
+  `content`   MEDIUMTEXT   NOT NULL,
+  `keywords`  VARCHAR(256) NOT NULL DEFAULT '',
+  `desc`      VARCHAR(256) NOT NULL DEFAULT '',
+  `is_show`   TINYINT(1)   NOT NULL DEFAULT '1',
+  `is_delete` TINYINT(1)   NOT NULL DEFAULT '0',
+  `is_top`    TINYINT(1)   NOT NULL DEFAULT '0',
+  `views`     INT(10)      NOT NULL DEFAULT '0',
+  `time`      INT(10)      NOT NULL DEFAULT '0',
+  `cid`       TINYINT(2)   NOT NULL DEFAULT '0',
   PRIMARY KEY (aid)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+-- ----------------------
+-- 用户
+-- ----------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_name`     VARCHAR(16) NOT NULL UNIQUE,
+  `pwd`           VARCHAR(32) NOT NULL,
+  `register_time` INT(10)     NOT NULL,
+  `avatar`        VARCHAR(255)         DEFAULT '',
+  `status`        TINYINT(1)  NOT NULL DEFAULT '1',
+  `email`         VARCHAR(20)          DEFAULT '',
+  `type`          TINYINT(1)           DEFAULT '2',
+  `login_times`   INT(6)               DEFAULT '0',
+  `last_login`    INT(10)     NOT NULL,
+  PRIMARY KEY (user_name)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
